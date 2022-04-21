106a4228766e6fc9089ec6cfbd75e0d20897840a
Mischka Booley <mischka@gearbox.com.au>
2022-04-21 19:30:23 +1000

    commit tree bf4d46cee1fb8c77868b6a4ed6e97faf0b7da8c4

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  // Write your code here! 
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 2500) {
+    return 'No can do.';
+  } else if (distance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+  return destination === 'NYC' ? ('Ok, sounds good.') : ('No go.');
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(amount){
   // Write your code here!
+  switch (amount) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    case 'thanks for everything':
+      return 'Bye.';
+  }
 }
